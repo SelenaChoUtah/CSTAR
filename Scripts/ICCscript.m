fda45afeebf2e1c526f84399879172b8fe9d9561
SelenaChoUtah <selena.cho@utah.edu>
2024-09-17 16:34:19 -0600

    updating more scripts

--- Scripts/ICCscript.m
+++ Scripts/ICCscript.m
@@ -46,33 +46,92 @@ for t = 1:length(task)
     compBaseline.(task{t}) = mean(compData.(task{t}),1);
 end
 
-%% Looping through diff day averages
-close all
-% task = {'amplitude','angVelocity','number'};
-taskName = 'number';
-fullData =  mean(compData.(taskName),2);
-for i = 1:5
-    partialData =  mean(compData.(taskName)(:,1:i),2);
-    compareData = [partialData,fullData];
+%% get step count for ICC
 
-    [r(i), LB(i), UB(i),~,~,~] = ICC(compareData, 'A-k');
-    % 
-    % ICC(i) = computeICC(partialData, fullData, '2-1');
-    % 
-    % % SDD
-    % meanActivity = mean([partialData, fullData]);
-    % difference = abs(partialData - fullData);
-    % SDD(i) = 1.96 * std(difference) / mean(meanActivity) * 100; % SDD as a percentage of the mean
+task = {'stepCount','meanStepBout'};
+id = fieldnames(data);
+j = 1;
+for i = 1:length(id)
+    try
+    dataStruct = 'stepData';
+    % check if there is at least 5 
+    % subDay(i,1) = length(fieldnames(data.(id{i}).(dataStruct)));
+    if length(fieldnames(data.(id{i}).(dataStruct))) >= 5
+        for t = 1:length(task)
+            dayNum = fieldnames(data.(id{i}).(dataStruct));
+            for d = 1:5
+                compData.(task{t})(j,d) = mean(data.(id{i}).(dataStruct).(dayNum{d}).waist.(task{t}));
+                % compData.number(j,d) = length(data.(id{i}).(dataStruct).(dayNum{d}).waist.(task{t}));
+            end
+        end
+        j = j+1;
+    end
+    catch
+        disp('No sensor')
+    end
 end
 
+% for t = 1:length(task)
+%     compBaseline.(task{t}) = mean(compData.(task{t}),1);
+% end
+
+%% Head on Trunk
+
+task = {'headOnTrunk'};
+id = fieldnames(data);
+j = 1;
+for i = 1:length(id)
+    try
+    dataStruct = 'headOnTrunkCount';
+    % check if there is at least 5 
+    % subDay(i,1) = length(fieldnames(data.(id{i}).(dataStruct)));
+    if length(fieldnames(data.(id{i}).(dataStruct))) >= 5
+        for t = 1:length(task)
+            dayNum = fieldnames(data.(id{i}).(dataStruct));
+            for d = 1:5
+                compData.(task{t})(j,d) = mean(data.(id{i}).(dataStruct).(dayNum{d}));
+                % compData.number(j,d) = length(data.(id{i}).(dataStruct).(dayNum{d}).waist.(task{t}));
+            end
+        end
+        j = j+1;
+    end
+    catch
+        disp('No sensor')
+    end
+end
+
+
+%% Looping through diff day averages
+close all
+task = {'amplitude','angVelocity','number','stepCount','headOnTrunk'};
+% taskName = 'number';
 figure
-plot(r)
-ylim([0 1.2])
-yline(0.7,'--')
-title(taskName)
+for t = 1:length(task)
+    hold on
+    for i = 1:5
+        partialData =  mean(compData.(task{t})(:,1:i),2);
+        fullData =  mean(compData.(task{t}),2);
+        compareData = [partialData,fullData];
+    
+        [r(i), LB(i), UB(i),~,~,~] = ICC(compareData, 'A-k');
+    end
+    plot(r)
+    
+end
+legend(task)
+title('Turning and Locomotion')
 xlabel('days')
 ylabel('ICC')
-saveas(gcf,append(taskName,'_icc'),'emf')
+saveas(gcf,append('amp_angVel_number_icc'),'emf')
+
+% figure
+% plot(r)
+% ylim([0 1.2])
+% yline(0.7,'--')
+% title(taskName)
+% xlabel('days')
+% ylabel('ICC')
+% saveas(gcf,append(taskName,'_icc'),'emf')
 
 %% Turns per hour ICC
 close all
